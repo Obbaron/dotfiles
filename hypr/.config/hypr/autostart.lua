7f72608abb31e7a098397b60f1e9ff80df20e14f
bnb <ronbelina@gmail.com>
2026-09-23 18:08:20 +0100

    added kdeconnect autostart

--- hypr/.config/hypr/autostart.lua
+++ hypr/.config/hypr/autostart.lua
@@ -4,5 +4,6 @@ hl.on("hyprland.start", function()
   -- daemon returns once shell is initialized
   -- polkit agent starts asynchronously just after UI
   hl.exec_cmd("noctalia --daemon")
+  ht.exec_cmd("/usr/lib/kdeconnectd")
   hl.exec_cmd("vicinae server")
 end)
